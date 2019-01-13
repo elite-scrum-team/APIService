052cb3f0943daf6431ef1e7b94d662f5854e8bc0
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-13 14:41:02 +0100

    fixed routes

--- src/routers/warning.js
+++ src/routers/warning.js
@@ -6,21 +6,21 @@ const router = express.Router();
 
 // get warnings
 router.get('/', async (req, res) => {
-    return await WarningService.warning.retrive(req.query);
+    await res.send(await WarningService.warning.retrive(req.query));
 });
 
 router.get('/:id', async (req, res) => {
-    return await WarningService.warning.retriveOne(req.params.id);
+    await res.send(await WarningService.warning.retriveOne(req.params.id));
 });
 
 // create warning
 router.post('/', async (req, res) => {
-    return await WarningService.warning.create(req.body);
+    await res.send(await WarningService.warning.create(req.body));
 });
 
 // create image
 router.post('/image', async (req, res) => {
-    return await WarningService.image.create(req.body);
+    await res.send(await WarningService.image.create(req.body));
 });
 
 module.exports = router;
