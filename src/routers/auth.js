0c153e5dbfdf3e5945ab62b8c4d3dcadfa562866
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-14 08:49:46 +0100

    added status

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -7,13 +7,13 @@ const router = express.Router();
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     const r = await UserService.token(email, password);
-    await res.send(await r.json());
+    await res.send(await r.json(), r.status);
 });
 
 router.post('/register', async (req, res) => {
     const user = req.body;
     const r = await UserService.register(user);
-    await res.send(r.json());
+    await res.send(r.json(), r.status);
 });
 
 module.exports = router;
