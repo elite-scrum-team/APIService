bc8167f018bd82ad6c16c71be288610aa3143bfb
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-14 13:57:46 +0100

    fixed non awaits

--- src/routers/warning.js
+++ src/routers/warning.js
@@ -13,25 +13,27 @@ router.post('/image', async (req, res) => {
 // get categories
 router.get('/category', async (req, res) => {
     const r = await WarningService.category.retrive();
-    await res.send(await r.json());
+    await res.send(await r.json(), r.status);
 });
 
 // get warnings
 router.get('/', async (req, res) => {
     const r = await WarningService.warning.retrive(req.query, req.userId);
-    await res.send(r.json(), r.status);
+    await res.send(await r.json(), r.status);
 });
 
 router.get('/:id', async (req, res) => {
-    await res.send(
-        await WarningService.warning.retriveOne(req.params.id, req.userId)
+    const r = await WarningService.warning.retriveOne(
+        req.params.id,
+        req.userId
     );
+    await res.send(await r.json(), r.status);
 });
 
 // create warning
 router.post('/', async (req, res) => {
     const r = await WarningService.warning.create(req.body, req.userId);
-    await res.send(r.json());
+    await res.send(await r.json(), r.status);
 });
 
 module.exports = router;
