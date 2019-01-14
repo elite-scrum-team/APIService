d7a9c9da5818be1138eb7e83f034949f9bdbfe4f
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-01-14 17:09:23 +0100

    Fixed status-endppint issue

--- src/util/auth.js
+++ src/util/auth.js
@@ -4,7 +4,9 @@ const jwt = require('jsonwebtoken');
 module.exports = {
     async validate(token) {
         try {
-            return jwt.verify(token, 'secret');
+            const decoded = jwt.verify(token, 'secret');
+            console.log("DECODED: ", decoded);
+            return decoded;
         } catch(err) {
             return undefined;    
         }
