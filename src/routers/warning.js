2f39652c053b1c1f0603f9a327955556809596f7
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-01-15 12:23:06 +0100

    Changed /warning/content endpoint

--- src/routers/warning.js
+++ src/routers/warning.js
@@ -41,9 +41,9 @@ router.get('/:id', async (req, res) => {
     await res.send(await r.json(), r.status);
 });
 
-router.get('/:id/content', async (req, res) => {
+router.get('/content', async (req, res) => {
     const result = await WarningService.warning.retrieveContent(
-        req.params.id,
+        req.query.id,
         req.userId
     );
     await res.send(await result.json(), result.status);
